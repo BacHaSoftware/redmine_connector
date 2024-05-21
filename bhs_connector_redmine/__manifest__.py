20bcf6bfb038af77944784a66067ea67d176702a
Nguyen.Thi.Phuong.Dung <dungntp.bhsoft@gmail.com>
2024-05-21 10:28:18 +0700

    v17: update banner

--- bhs_connector_redmine/__manifest__.py
+++ bhs_connector_redmine/__manifest__.py
@@ -20,7 +20,7 @@
     'external_dependencies': {
         'python': ['python-redmine'],
     },
-    'images': ['static/images/banner.gif'],
+    'images': ['static/description/banner.gif'],
     'installable': True,
     'application': True,
     'license': 'LGPL-3'
